e876d930a1a4ecd08d13ad2aac629808a7b37180
Albert <albertwu0001@gmail.com>
2020-06-20 16:34:26 -0700

    refactor code

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,3 +1,9 @@
+console.ignoredYellowBox = ['Remote debugger'];
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings([
+  'Unrecognized WebSocket connection option(s) `agent`, `perMessageDeflate`, `pfx`, `key`, `passphrase`, `cert`, `ca`, `ciphers`, `rejectUnauthorized`. Did you mean to put these under `headers`?',
+]);
+
 import React from 'react';
 import HomeScreen from './screens/HomeScreen';
 
