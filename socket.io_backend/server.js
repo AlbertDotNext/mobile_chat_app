02e81d0e38c4d5b822b7a82f588feda668543aa7
Albert <albertwu0001@gmail.com>
2020-06-22 14:28:20 -0700

    add uuid

--- socket.io_backend/server.js
+++ socket.io_backend/server.js
@@ -1,28 +1,42 @@
 // const express = require('express');
 const io = require('socket.io')();
+const messageHandler = require('./handlers/message_handler');
+const uuidv1 = require('uuid/v1');
 
-let currentUserId = 2;
-let currentMessageId = 1;
-const userIds = {};
+const users = {};
 
-function createMessage(userId, messageText) {
-  return {
-    _id: currentMessageId++,
-    text: messageText,
-    createdAt: new Date(),
-    user: {
-      _id: userId,
-      name: 'Test User',
-      avatar:
-        'https://pic2.cwuzx.com/e1e1cbefbffab1ee214ee9e98da2672748115d11-800.jpg',
-    },
-  };
+function createdUserAvatarUrl() {
+  const rand1 = Math.round(Math.random() * 200 + 100);
+  const rand2 = Math.round(Math.random() * 200 + 100);
+  return `http://placeimg.com/${rand1}/${rand2}/any`;
 }
-
 io.on('connection', (socket) => {
   console.log('a user connected!');
   console.log(socket.id);
-  userIds[socket.id] = currentUserId++;
+  users[socket.id] = { userId: uuidv1() };
+  socket.on('join', (username) => {
+    users[socket.id].username = username;
+    users[socket.id].avatar = createdUserAvatarUrl();
+    messageHandler.handleMessage(socket, users);
+  });
+  socket.on('action', (action) => {
+    switch (action.type) {
+      case 'server/hello':
+        console.log('Got hello event', action.data);
+        socket.emit('action', { type: 'message', data: 'Good day!' });
+        break;
+      case 'server/join':
+        console.log('Got join event', action.data);
+        users[socket.id].username = action.data;
+        users[socket.id].avatar = createdUserAvatarUrl();
+        const values = Object.values(users);
+        const onlyWithUsernames = values.filter(
+          (u) => u.username !== undefined
+        );
+        io.emit('action', { type: 'user_onlline', data: value });
+        break;
+    }
+  });
 });
 
 const PORT = process.env.PORT || 3001;
