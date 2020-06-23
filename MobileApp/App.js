a0a84052e2567c57741b211658d125b46bc114cb
Albert <albertwu0001@gmail.com>
2020-06-22 17:48:07 -0700

    add conversations

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -13,10 +13,21 @@ import { Provider } from 'react-redux';
 const socket = io('http://192.168.1.5:3001');
 const socketIoMiddleware = createSocketIoMiddleware(socket, 'server/');
 
-function reducer(state = {}, action) {
+function reducer(state = { conversations: {} }, action) {
   switch (action.type) {
     case 'users_online':
-      return { ...state, usersOnline: action.data };
+      const conversations = { ...state.conversations };
+      const usersOnline = action.data;
+      for (let i = 0; i < usersOnline.length; i++) {
+        const userId = usersOnline[i].userId;
+        if (conversations[userId] === undefined) {
+          conversations[userId] = {
+            messages: [],
+            username: usersOnline[i].username,
+          };
+        }
+      }
+      return { ...state, usersOnline, conversations };
     case 'private_message':
       const conversationId = action.data.conversationId;
       return {
