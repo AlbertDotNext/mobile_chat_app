132603112e2faf7a1e4521008858b2e7e131ef72
Albert <albertwu0001@gmail.com>
2020-06-22 17:23:20 -0700

    refactor code for cases

--- socket.io_backend/server.js
+++ socket.io_backend/server.js
@@ -1,7 +1,7 @@
 // const express = require('express');
 const io = require('socket.io')();
 const messageHandler = require('./handlers/message_handler');
-const uuidv1 = require('uuid/v1');
+const { v1: uuidv1 } = require('uuid');
 
 const users = {};
 
@@ -10,31 +10,33 @@ function createdUserAvatarUrl() {
   const rand2 = Math.round(Math.random() * 200 + 100);
   return `http://placeimg.com/${rand1}/${rand2}/any`;
 }
+
+function createUsersOnline() {
+  const values = Object.values(users);
+  const onlyWithUsernames = values.filter((u) => u.username !== undefined);
+  return onlyWithUsernames;
+}
+
 io.on('connection', (socket) => {
   console.log('a user connected!');
   console.log(socket.id);
   users[socket.id] = { userId: uuidv1() };
-  socket.on('join', (username) => {
-    users[socket.id].username = username;
-    users[socket.id].avatar = createdUserAvatarUrl();
-    messageHandler.handleMessage(socket, users);
+
+  socket.on('disconnect', () => {
+    delete users[socket.id];
+    io.emit('action', { type: 'users_online', data: createUsersOnline() });
   });
   socket.on('action', (action) => {
     switch (action.type) {
-      case 'server/hello':
-        console.log('Got hello event', action.data);
-        socket.emit('action', { type: 'message', data: 'Good day!' });
-        break;
       case 'server/join':
         console.log('Got join event', action.data);
         users[socket.id].username = action.data;
         users[socket.id].avatar = createdUserAvatarUrl();
-        const values = Object.values(users);
-        const onlyWithUsernames = values.filter(
-          (u) => u.username !== undefined
-        );
-        io.emit('action', { type: 'user_onlline', data: value });
+        io.emit('action', { type: 'users_online', data: createUsersOnline() });
+        socket.emit('action', { type: 'self_user', data: users[socket.id] });
         break;
+      case 'server/private-message':
+        console.log('Got a private-message', action.data);
     }
   });
 });
