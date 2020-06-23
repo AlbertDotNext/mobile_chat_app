85c2dc582e01af3d07f3d3e92ed5fc1c1d54fdc6
Albert <albertwu0001@gmail.com>
2020-06-22 17:48:51 -0700

    dispatch private message

--- MobileApp/screens/ChatScreen.js
+++ MobileApp/screens/ChatScreen.js
@@ -9,21 +9,27 @@ ChatScreen.navigationOptions = (screenProps) => ({
   title: screenProps.navigation.getParam('name'),
 });
 
-export default function ChatScreen() {
+export default function ChatScreen({ navigation }) {
   const dispatch = useDispatch();
   const selfUser = useSelector((state) => state.selfUser);
+  const conversations = useSelector((state) => state.conversations);
   const userId = navigation.getParam('userId');
+  const messages = conversations[userId].messages;
   return (
     <View style={{ flex: 1 }}>
       <GiftedChat
         renderUsernameOnMessage
-        messages={[]}
-        onSend={(messages) =>
+        messages={messages}
+        onSend={(messages) => {
           dispatch({
             type: 'private_message',
             data: { message: messages[0], conversationId: userId },
-          })
-        }
+          });
+          dispatch({
+            type: 'server/private_message',
+            data: { message: messages[0], conversationId: userId },
+          });
+        }}
         user={{
           _id: selfUser,
         }}
